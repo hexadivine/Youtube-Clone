84e96f67ca6264d1ecf01ab924412d440b400877
hexadivine <hexadivine@gmail.com>
2024-07-27 19:35:05 +1000

    added quick suggestion buttons bar

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const toggleHideElements = document.querySelectorAll(".toggle-hide");
 const toggleMenuBar = document.getElementById("toggleMenuBar");
+const mainContainer = document.getElementById("mainContainer");
 
 function toggleMenu() {
     toggleHideElements.forEach(function (toggleHideElement) {
         toggleHideElement.classList.toggle("hide");
     });
-    toggleMenuBar.classList.toggle("small-menu-bar")
+    toggleMenuBar.classList.toggle("small-menu-bar");
+    mainContainer.style.width = "calc(100% - 10.62rem)"
 }
